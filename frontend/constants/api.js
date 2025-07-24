0029441eb78909a8b88ed78a7948d4c9edddfdd6
madanpiske3 <madanpiske1729@gmail.com>
2025-07-24 10:28:56 +0530

    wednesday-morning-24-7

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://192.168.29.222:3000/api";
-export const API_URL = "http://192.168.1.2:3000/api";
+export const API_URL = "http://192.168.1.7:3000/api";
 
 
 // type ipconfig on command prompt (cmd) to get your IPV4 address
